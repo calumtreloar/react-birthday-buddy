59db219c6b71139e700f3e830ddedc2309c716d4
calumtreloar <calumtreloar@gmail.com>
2024-03-07 15:58:53 +1030

    Initial commit

---
+++ src/Person.jsx
@@ -0,0 +1,11 @@
+const Person = ({ name, age, image }) => {
+  return (
+    <article className="person">
+      <img src={image} alt={name} className="img" />
+      <h4>{name}</h4>
+      <p>{age} years</p>
+    </article>
+  );
+};
+
+export default Person;
